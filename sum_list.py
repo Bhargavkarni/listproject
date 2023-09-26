f2e412331840658f79f68897d1a56f801dffd242
BHARGAV <bhargavkarni.marolix@gmail.com>
2023-09-26 12:40:52 +0530

    sum_list

---
+++ sum_list.py
@@ -0,0 +1,16 @@
+# test_case1 = [8,10,12]
+# total = 0
+#
+# for each_num in range(0,len(test_case1)):
+#     total = total + test_case1[each_num]
+#
+# print(total)
+
+
+test_case = [1,4,6,7]
+sum = 0
+
+for i in test_case:
+    sum = sum + i
+
+print(sum)
